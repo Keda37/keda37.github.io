4d1e6ee3a848a25018aa3cb7526dd9f061c3356a
Keda37 <fakesupp@gmail.com>
2018-04-11 18:25:54 +0300

    add dark theme in sass variables, remove svg img and add svg img in html

--- script/script.js
+++ script/script.js
@@ -18,12 +18,8 @@ function doScrolling(elementY, duration) {
 var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
 
 anchors.forEach(function(item) {
-  // каждому якорю присваиваем обработчик события
   item.addEventListener('click', function(e) {
-    // убираем стандартное поведение
     e.preventDefault();
-    
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
     var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
     
     doScrolling(coordY, 700);    
